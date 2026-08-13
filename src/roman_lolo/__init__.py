6a94ce2c6d9145b95dc580c118eb3b5993813779
Maxime Rizzo <maxime.rizzo@gmail.com>
2026-08-12 22:51:35 -0400

    upgraded version

--- src/roman_lolo/__init__.py
+++ src/roman_lolo/__init__.py
@@ -1,20 +1,18 @@
 """Roman WFI CRNL simulation and photometry tools."""
 
-from .roman_crnl_sim import (
-    RomanWFIParams,
-    CRNLModel,
-    SceneGenerator,
-    LampOnOffSimulator,
-    plot_scene,
-    plot_crnl_curve,
-    save_to_fits,
-    save_dithered_to_fits,
-    simulate_LOLO_dataset,
-    plot_lamp_differences,
-    generate_flat_field,
-)
-
 from .romanphot import SourcePhotometry
+
+
+def __getattr__(name):
+    _crnl_sim_exports = {
+        'RomanWFIParams', 'CRNLModel', 'SceneGenerator', 'LampOnOffSimulator',
+        'plot_scene', 'plot_crnl_curve', 'save_to_fits', 'save_dithered_to_fits',
+        'simulate_LOLO_dataset', 'plot_lamp_differences', 'generate_flat_field',
+    }
+    if name in _crnl_sim_exports:
+        from . import roman_crnl_sim as _m
+        return getattr(_m, name)
+    raise AttributeError(f"module 'roman_lolo' has no attribute {name!r}")
 
 __all__ = [
     'RomanWFIParams',
@@ -31,4 +29,4 @@ __all__ = [
     'generate_flat_field',
 ]
 
-__version__ = '0.1.0'
+__version__ = '0.3.0'
